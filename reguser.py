09d1dc67f4977a3e86e03eca721a9690497138aa
QuantumaStelata <quantumastelata@gmail.com>
2020-08-09 13:31:35 +0000

    Bag-fix

--- reguser.py
+++ reguser.py
@@ -5,6 +5,9 @@ import os
 def reg(message):
     if str(message.chat.id) not in const.copy.keys():
         with open('base.json', 'w', encoding="utf-8") as ff:
-            os.mkdir('voice/{}/'.format(str(message.chat.id)))
+            try:
+                os.mkdir('voice/{}/'.format(str(message.chat.id)))
+            except:
+                pass
             const.copy[str(message.chat.id)] = {"1Name": str(message.from_user.first_name), "2Name": str(message.from_user.last_name), "ID": str(message.from_user.username),"Voice": "voice/{}/".format(str(message.chat.id)),"TZ": str(const.timezone), "Work": {}}
             json.dump(const.copy, ff, sort_keys=True, indent=4, ensure_ascii=False)
