75dc93ab0515af0db3103d373f7dfc0b4003ff48
QuantumaStelata <quantumastelata@gmail.com>
2020-08-17 23:39:56 +0000

    тест 1

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 import sqlite3
+
+with sqlite3.connect('base.db') as db:
+    cur = db.cursor()
+
+    #cur.execute(u"""INSERT INTO base VALUES (403689973, 'Vladyslav', 'None', 'QuantumaStelata', 1390, 3, 'voice/403689972/', '{1:2}', 'None')""")
+    cur.execute(u"""SELECT remindtime FROM base""")
+    for i in cur.fetchall():
+        for j in i:
+            print (type(eval(j)))
+
